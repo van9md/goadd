06ed9147935028116817be83b067e7fee374093e
van9 <van9md@gmail.com>
2024-07-05 16:42:22 +0300

    v1.0.2

--- goadd.go
+++ goadd.go
@@ -1,5 +1,8 @@
+//#Package by van9
 package goadd
-
+//Add adds two int numbers together and return int
+//
+//https://www.mathsisfun.com/numbers/addition.html
 func Add(x,y int) int {
     return x+y
 }
